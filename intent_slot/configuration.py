3a707c38bed2db08e33c346edca9f7a0d66fd62b
Dave Zornek <david.zornek@gmail.com>
2023-06-12 16:12:20 -0500

    Convert tabs to spaces

--- intent_slot/configuration.py
+++ intent_slot/configuration.py
@@ -4,13 +4,14 @@ import yaml
 
 @dataclass
 class IntentSlotConfig:
-	"""Class for configuring an intent-slot model training experiment"""
-	name: str
-	base_model: str
-	data: str
-	max_len: int
+    """Class for configuring an intent-slot model training experiment"""
+
+    name: str
+    base_model: str
+    data: str
+    max_len: int
 
 
 def load_yaml_config(path):
-     with open(path, "r") as f:
+    with open(path, "r") as f:
         return yaml.safe_load(f)
--- intent_slot/data.py
+++ intent_slot/data.py
@@ -14,17 +14,17 @@ class IntentSlotDataProcessor(DataProcessor):
             self.train_data = self.data["train"]
         except KeyError:
             self.train_data = None
-            
+
         try:
             self.val_data = self.data["validation"]
         except KeyError:
             self.val_data = None
-        
+
         try:
             self.test_data = self.data["test"]
         except KeyError:
             self.test_data = None
-        
+
     def process_data(self):
     	"""Performas any preprocessing and tokenization necessary"""
         if self.train_data:
@@ -33,18 +33,18 @@ class IntentSlotDataProcessor(DataProcessor):
             self.processed_val_data = self._tokenize_dataset(self.val_data)
         if self.test_data:
             self.processed_test_data = self._tokenize_dataset(self.test_data)
-    
+
     def _tokenize_dataset(self, dataset):
-    	"""tokenizes data and aligns labels to account for word splitting and 
+    	"""tokenizes data and aligns labels to account for word splitting and
     	special tokens"""
         tokenized_inputs = self.tokenizer(dataset["tokens"], is_split_into_words=True)
-        
+
         labels = []
         for i, label in enumerate(dataset[f"ner_tags"]):
             word_ids = tokenized_inputs.word_ids(batch_index=i)  # Map tokens to their respective word.
             previous_word_idx = None
             label_ids = []
-            for word_idx in word_ids:  
+            for word_idx in word_ids:
                 if word_idx is None: # Set the special tokens to -100.
                     label_ids.append(-100)
                 elif word_idx != previous_word_idx:  # Only label the first token of a given word.
@@ -53,7 +53,7 @@ class IntentSlotDataProcessor(DataProcessor):
                     label_ids.append(-100)
                 previous_word_idx = word_idx
             labels.append(label_ids)
-            
+
         tokenized_inputs["labels"] = labels
-        
+
         return tokenized_inputs
